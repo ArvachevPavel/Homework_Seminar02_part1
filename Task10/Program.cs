8de5c9d5f6684a2bb78327b2b8cce53c73048b0a
Pavel Arvachev <110897986+ArvachevPavel@users.noreply.github.com>
2022-08-24 20:54:39 +0300

    Add files via upload

---
+++ Task10/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+int number = 99;
+
+while (number < 100 || number >=1000)
+{
+    Console.WriteLine("Введите трехзначное число: ");
+    number = int.Parse(Console.ReadLine());
+}
+int a = number / 10;
+int b = number / 100;
+Console.WriteLine("Вторая цифра в этом числе это " + (a - b*10));
+
+
+
---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+int number = 0;
+while (number < 1 || number >=8)
+{
+    Console.WriteLine("Введите число от 1 до 7: ");
+    number = int.Parse(Console.ReadLine());
+}
+if (number == 6 || number == 7)
+{
+    Console.WriteLine("Выходной день");
+}
+else
+{
+    Console.WriteLine("Невыходной день");
+}
